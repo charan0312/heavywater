970ccfa126e9ae82dc811cd2cbb7ed02bc076471
Charan Parvathareddy <charan03.12@gmail.com>
2019-05-21 17:19:49 -0500

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install python3-pip -y
-sudo pip3 install -r requirements
-git clone https://github.com/charan0312/heavywater.git
+sudo pip3 install -r requirements.txt
 cd webapp
 nohup python3 app.py &
